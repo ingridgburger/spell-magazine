4840ce97d94e7542faddcdad8da60804f63b0afa
Ingrid Burger <igb34@drexel.edu>
2026-09-22 13:57:19 -0400

    add wr 2026 workshop

--- src/data/editions.js
+++ src/data/editions.js
@@ -4,10 +4,13 @@ import winter2025 from "../assets/images/spell-winter-2025.webp";
 import backFallWinter2026 from "../assets/images/back-fall-winter-2026.webp";
 import backSpring2025 from "../assets/images/back-spring-2025.webp";
 import backWinter2025 from "../assets/images/back-winter-2025.webp";
-import zineWorkshop from "../assets/images/zine-workshop.webp";
-import backZineWorkshop from "../assets/images/back-zine-workshop.webp";
+import zineWorkshop2025 from "../assets/images/zine-workshop.webp";
+import backZineWorkshop2025 from "../assets/images/back-zine-workshop.webp";
 import springSummer2026 from "../assets/images/spell-spring-summer-2026.webp";
 import backSpringSummer2026 from "../assets/images/back-spell-spring-summer-2026.webp";
+import zineWorkshop2026 from "../assets/images/wr-spell-cover.webp";
+import backZineWorkshop2026 from "../assets/images/wr-spell-back-cover.webp";
+
 
 export const editions = [
   {
@@ -23,13 +26,13 @@ export const editions = [
     printAvailable: true,
   },
   {
-    id: "zine-workshop",
-    label: "WR x SPELL Zine Workshop",
-    title: "WR x SPELL Zine Workshop",
-    image: zineWorkshop,
-    backImage: backZineWorkshop,
-    openInNewTabUrl: "https://heyzine.com/flip-book/8e21b54687.html",
-    embedUrl: "https://heyzine.com/flip-book/8e21b54687.html",
+    id: "zine-workshop-2026",
+    label: "WR x SPELL Zine Workshop 2026",
+    title: "WR x SPELL Zine Workshop 2026",
+    image: zineWorkshop2026,
+    backImage: backZineWorkshop2026,
+    openInNewTabUrl: "https://heyzine.com/flip-book/789d034643.html",
+    embedUrl: "https://heyzine.com/flip-book/789d034643.html",
     printAvailable: false,
     disclaimer:
       "We hosted a hands-on zine-making station during the Writers Room Open House, inviting incoming Drexel students to dive into the world of art, publishing, and DIY creativity through collage, writing, and self-expression. Explore pages from our zine workshop below!",
@@ -46,6 +49,18 @@ export const editions = [
     printOrderUrl:
       "https://docs.google.com/forms/d/e/1FAIpQLSdJQBFUEoO9F1VgkQj24ipuMDvtfLZgmlGmielzsEMi2PALRA/viewform?usp=send_form",
     printAvailable: true,
+  },
+  {
+    id: "zine-workshop-2025",
+    label: "WR x SPELL Zine Workshop 2025",
+    title: "WR x SPELL Zine Workshop 2025",
+    image: zineWorkshop2025,
+    backImage: backZineWorkshop2025,
+    openInNewTabUrl: "https://heyzine.com/flip-book/8e21b54687.html",
+    embedUrl: "https://heyzine.com/flip-book/8e21b54687.html",
+    printAvailable: false,
+    disclaimer:
+      "We hosted a hands-on zine-making station during the Writers Room Open House, inviting incoming Drexel students to dive into the world of art, publishing, and DIY creativity through collage, writing, and self-expression. Explore pages from our zine workshop below!",
   },
   {
     id: "spring-2025",
